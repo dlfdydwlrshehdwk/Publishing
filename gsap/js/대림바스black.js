9681dc6ea52290f233df837ba8c465ff86f26848
dongho <dotlqksk@gmail.com>
2025-07-09 18:16:22 +0900

    45

--- gsap/js/대림바스black.js
+++ gsap/js/대림바스black.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-  
+
   gsap.registerPlugin(ScrollTrigger);
 
   // 1. Lenis 인스턴스 생성
@@ -33,7 +33,6 @@ document.addEventListener('DOMContentLoaded', function(){
   function calcWindowH (num = 1){
     return (num * windowH).toString();
   }
-  console.log(calcWindowH(3))
 
   // section1 -> section2 
   ScrollTrigger.create({
@@ -60,7 +59,6 @@ document.addEventListener('DOMContentLoaded', function(){
     scrub: true,
     pin: true,
     markers:true,
-    // pinType: 'transform',
     anticipatePin: 1,
     onUpdate: (self) => {
       const progress = self.progress;
@@ -74,27 +72,36 @@ document.addEventListener('DOMContentLoaded', function(){
   ScrollTrigger.create({
     trigger: ".section3",
     start: `top ${headerH}`,
-    end: calcWindowH(4),
+    end: calcWindowH(5),
     scrub: true,
     pin: true,
     markers:true,
-    // pinType: 'transform',
     anticipatePin: 1,
     onUpdate: (self) => {
       const progress = self.progress;
-      console.log(progress)
+
+      // 이미지 위로
+      const maxYPercent = -24.62;
+      const yValue = progress * maxYPercent;
+      gsap.set('.content3 .bg', {
+          yPercent: yValue
+      });
+
+      // 타이틀 on 초기화
       section3TitleSet.forEach(ele => {
         ele.classList.remove('on')
       })
+      
+      // 타이틀 on 토글
       if(progress < 0.25) {
         section3TitleSet[0].classList.add('on')
       } else if (progress < 0.5) {
         section3TitleSet[1].classList.add('on')
       } else if (progress < .75) {
         section3TitleSet[2].classList.add('on')
-
       }
 
+      // 화면전환
       if (progress < 0.75) {
           gsap.to('.content3', { opacity: 1 });
           gsap.to('.content4', { opacity: 0 });
@@ -115,12 +122,62 @@ document.addEventListener('DOMContentLoaded', function(){
     scrub: true,
     pin: true,
     markers:true,
-    // pinType: 'transform',
     anticipatePin: 1,
     onUpdate: (self) => {
       const progress = self.progress;
       gsap.to('.content4', {opacity: 1 - progress});
       gsap.to('.content5', {opacity: progress});
+    }
+  })
+
+  // section5 -> section6 
+  const section5Item1 = document.querySelector('.content5 .item1 .main_image');
+  const section5Item2 = document.querySelector('.content5 .item2 .main_image');
+  const section5Item3 = document.querySelector('.content5 .item3 .main_image');
+  const section5Line1 = document.querySelector('.content5 .item1 .line');
+  const section5Line2 = document.querySelector('.content5 .item1 .line');
+  const section5Line3 = document.querySelector('.content5 .item1 .line');
+  const section6Length = 7;
+  const section6Split = 100 / 7;
+  console.log('0',section6Split)
+  console.log('1',section6Split * (section6Length - 1))
+  ScrollTrigger.create({
+    trigger: ".section5",
+    start: `top ${headerH}`,
+    end: calcWindowH(section6Length),
+    scrub: true,
+    pin: true,
+    markers:true,
+    anticipatePin: 1,
+    onUpdate: (self) => {
+      const progress = self.progress;
+      console.log(progress)
+
+      // 아이템 등장
+      if(progress < section6Split) {
+        gsap.to(section5Item1, {opacity:1 });
+      } else if (progress < section6Split * 2) {
+        gsap.to(section5Item2, {opacity:1 });
+      } else if (progress < section6Split * 3) {
+        gsap.to(section5Item3, {opacity:1 });
+      } else if (progress < section6Split * 4) {
+        gsap.to(section5Line1, {opacity:1 });
+      } else if (progress < section6Split * 5) {
+        gsap.to(section5Line2, {opacity:1 });
+      } else if (progress < section6Split * 6) {
+        gsap.to(section5Line3, {opacity:1 });
+      }
+
+      // 화면전환
+      if (progress < section6Split * (section6Length - 1)) {
+          gsap.to('.content5', { opacity: 1 });
+          gsap.to('.content6', { opacity: 0 });
+      } else {
+          // progress 리매핑
+          const localProgress = (progress - section6Split * (section6Length - 1)) / section6Split;
+          gsap.to('.content5', { opacity: 1 - localProgress });
+          gsap.to('.content6', { opacity: localProgress });
+      }
     }
   })
 
