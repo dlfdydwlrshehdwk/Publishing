474e8006794f09cb59a55d938bb2ff2ce20ad69f
kdh <fkrmsk147@naver.com>
2025-08-07 02:59:05 +0900

    1

---
+++ portfolio/js/spc copy.js
@@ -0,0 +1,429 @@
+gsap.registerPlugin(ScrollTrigger);
+
+//모바일 브라우저 감지
+function isMobileBrowser() {
+// User Agent 체크 (가장 확실한 방법)
+const userAgent = navigator.userAgent.toLowerCase();
+const mobileKeywords = ['android', 'iphone', 'ipad', 'ipod', 'blackberry', 'windows phone', 'mobile'];
+const isMobileUA = mobileKeywords.some(keyword => userAgent.includes(keyword));
+
+// 터치 지원 여부
+const hasTouch = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
+// 화면 크기
+const isSmallScreen = window.innerWidth <= 768;
+
+// 모바일 기기인지 확인
+const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+return isMobileUA || isMobileDevice || (hasTouch && isSmallScreen);
+}
+
+const section03Swiper = new Swiper(".section3 .swiper", {
+    slidesPerView: 'auto',
+    spaceBetween: 11.5,
+    loop: true,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+    speed: 7000, 
+    768: {
+            spaceBetween: 20,
+    },
+    1024: {
+    },
+})
+section03Swiper.autoplay.start();
+gsap.set([
+    ".section2 .left",
+    ".section2 .right > .flex",
+    ".section2 .right .text_set",
+    '.section3 .logo',
+    '.section3 .title',
+    '.section3 .desc',
+    '.section4 .pc .item',
+    '.section4 .pc .item2',
+    '.section4 .pc .right', 
+    '.section4 .pc .item3', 
+    '.section4 .pc .item4',
+    '.section5 .text_set .title',
+    '.section5 .text_set .desc',
+    '.section5 .inner > .right',
+    '.section5 .top > .bottom',
+    '.section6 .text_set .title',
+    '.section6 .text_set .desc',
+    '.section6 .img_cont',
+    '.section6 .img_cont .img_set.side',
+    '.section7 .item1',
+    '.section7 .item2',
+    '.section7 .item3',
+    '.section7 .item4',
+    '.section7 .item5',
+    '.section8 .img_wrap',
+    '.section8 .text_set2'
+], {opacity: 0, y: 50})
+
+// section2
+gsap.to(".section2 .left", {
+    scrollTrigger: {
+        trigger: ".section2",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to(".section2 .right > .flex", {
+    scrollTrigger: {
+        trigger: ".section2",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to(".section2 .right .text_set", {
+    scrollTrigger: {
+        trigger: ".section2",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+
+// section3
+gsap.to(".section3 .logo", {
+    scrollTrigger: {
+        trigger: ".section3",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to(".section3 .title", {
+    scrollTrigger: {
+        trigger: ".section3",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to(".section3 .desc", {
+    scrollTrigger: {
+        trigger: ".section3",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+
+// section4
+gsap.to(".section4 .pc .item", {
+    scrollTrigger: {
+        trigger: ".section4",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to(".section4 .pc .item2", {
+    scrollTrigger: {
+        trigger: ".section4",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to(".section4 .pc .right", {
+    scrollTrigger: {
+        trigger: ".section4",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to(".section4 .pc .item3", {
+    scrollTrigger: {
+        trigger: ".section4",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to(".section4 .pc .item4", {
+    scrollTrigger: {
+        trigger: ".section4",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+
+// section05
+const card = document.querySelector('.section5 .card');
+const cardH = card.clientHeight;
+const cardWrap2Mt = window.getComputedStyle(document.querySelector('.section5 .card_wrap2')).marginTop;
+const cardWrapGap = window.getComputedStyle(document.querySelector('.section5 .card_wrap2')).gap
+const numCardWrap2Mt = Number(cardWrap2Mt.replace(/[^\d]/g, ''));
+const numCardWrapGap = Number(cardWrapGap.replace(/[^\d]/g, ''));
+
+gsap.to('.section5 .text_set .title', {
+    scrollTrigger: {
+        trigger: ".section5",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to('.section5 .text_set .desc', {
+    scrollTrigger: {
+        trigger: ".section5",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to('.section5 .inner > .right', {
+    scrollTrigger: {
+        trigger: ".section5",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to('.section5 .top > .bottom', {
+    scrollTrigger: {
+        trigger: ".section5",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+
+// section05Tl2
+const section05Tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.section5 .card_wrap2',
+        start: 'top center',
+        end: 'bottom center',
+        scrub: true,
+    }
+})
+section05Tl2 
+.to('.section5 .card.primary', {
+    left: 0,
+    x: 0,
+    top: cardH * 2 + numCardWrap2Mt,
+    duration: 1
+})
+.to('.section5 .card.sub', {
+    left: 0,
+    x: cardH + numCardWrapGap,
+    xPercent: 0,
+    top: cardH * 2 + numCardWrap2Mt,
+    duration: 1
+})
+.to('.section5 .card.yellow', {
+    left: 0,
+    x: (cardH * 2) + (numCardWrapGap * 2),
+    xPercent: 0,
+    top: cardH * 2 + numCardWrap2Mt,
+    duration: 1
+})
+.to('.section5 .card.light_gray', {
+    left: 0,
+    x: (cardH * 3) + (numCardWrapGap * 3),
+    xPercent: 0,
+    top: cardH * 2 + numCardWrap2Mt,
+    duration: 1
+})
+.to('.section5 .card.black', {
+    left: 0,
+    x: (cardH * 4) + (numCardWrapGap * 4),
+    xPercent: 0,
+    top: cardH * 2 + numCardWrap2Mt,
+    duration: 1
+})
+
+// section06
+gsap.to('.section6 .text_set .title', {
+    scrollTrigger: {
+        trigger: ".section6",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to('.section6 .text_set .desc', {
+    scrollTrigger: {
+        trigger: ".section6",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to('.section6 .img_cont', {
+    scrollTrigger: {
+        trigger: ".section6",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to('.section6 .img_cont .img_set.side', {
+    scrollTrigger: {
+        trigger: ".section6",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+
+// section07
+gsap.to('.section7 .item1', {
+    scrollTrigger: {
+        trigger: ".section7",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to('.section7 .item2', {
+    scrollTrigger: {
+        trigger: ".section7",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to('.section7 .item3', {
+    scrollTrigger: {
+        trigger: ".section7",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to('.section7 .item4', {
+    scrollTrigger: {
+        trigger: ".section7",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to('.section7 .item5', {
+    scrollTrigger: {
+        trigger: ".section7",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+
+
+// section08
+gsap.to('.section8 .img_wrap1', {
+    scrollTrigger: {
+        trigger: ".section8",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to('.section8 .img_wrap2', {
+    scrollTrigger: {
+        trigger: ".section8",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
+gsap.to('.section8 .text_set2', {
+    scrollTrigger: {
+        trigger: ".section8",
+        start: "top center",
+        end: "bottom center",
+        pin: false,
+    },
+    opacity: 1,
+    y:0,
+    duration: 1
+});
