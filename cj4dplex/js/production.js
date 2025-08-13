2c014ec701c0c00144de598e86e372e051cd2521
dongho <dotlqksk@gmail.com>
2025-08-13 18:41:28 +0900

    1

--- cj4dplex/js/production.js
+++ cj4dplex/js/production.js
@@ -79,3 +79,128 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 });
+
+
+// $(() => {
+//   const SEC03_SELECTOR = '.production .sec03';
+//   const CONT_SELECTOR = '.production .sec02 .perspective_slider .stage .cont';
+//   const SLIDE_SELECTOR = '.production .sec02 .perspective_slider .stage .cont .ring .slide';
+
+//   // 섹션3 3d캐러셀
+//   function setupCarousel() {
+//       const $slides = $(SLIDE_SELECTOR);
+//       if (!$slides.length) return;
+
+//       const slideCount = $slides.length;
+
+//       // 슬라이드의 실제 width(=height, 1:1 가정)
+//       const slideSize = $slides.eq(0).outerWidth();
+//       const desiredGap = 20; // px
+//       const angle = 360 / slideCount;
+
+//       // 반지름 계산
+//       const radius = ((slideSize + desiredGap) / 2) / Math.tan(Math.PI / slideCount);
+
+//       // 슬라이드 배치
+//       $slides.each(function(i){
+//           const slideAngle = i * angle;
+//           $(this).css('transform', 'rotateY(' + (slideAngle + 180) + 'deg) translateZ(' + (-radius) + 'px)');
+//       });
+
+//       // sec03 위치 조정
+//       requestAnimationFrame(() => {
+//         applySec03OffsetByRect();
+//       });
+//   }
+
+//   // sec03 위치 조정
+//   function applySec03OffsetByRect(){
+//     const $cont = $(CONT_SELECTOR);
+//     const contEl = $cont.get(0);
+//     if (!contEl) return;
+
+//     const wrapperHeight = $cont.outerHeight();
+//     const contTop = contEl.getBoundingClientRect().top;
+
+//     // 가장 멀리있는 슬라이드 찾기
+//     const slides = $(SLIDE_SELECTOR).get();
+//     if (!slides.length) return;
+
+//     let backEl = slides[0];
+//     let minH = slides[0].getBoundingClientRect().height;
+
+//     for (let i = 1; i < slides.length; i++){
+//       const h = slides[i].getBoundingClientRect().height;
+//       if (h < minH) { minH = h; backEl = slides[i]; }
+//     }
+
+//     const backRect = backEl.getBoundingClientRect();
+//     const bottomY  = backRect.bottom - contTop;
+//     const marginTop = -(wrapperHeight - bottomY);
+
+//     $(SEC03_SELECTOR).css('margin-top', marginTop + 'px');
+//   }
+
+//   let resizeTimer = null;
+//   $(window).on('resize', function(){
+//       if (resizeTimer) clearTimeout(resizeTimer);
+//       resizeTimer = setTimeout(setupCarousel, 100);
+//   });
+  
+//   setupCarousel();
+
+
+//   // 섹션1 텍스트 애니 
+//   $(".page_title").addClass("visible"); // 임시 작업
+
+//   const subTitle = $('.title_wrap .sub_trans'),
+//         tranLastE = $('.trans').last();
+//   var eDelayTime = tranLastE.css('transition-delay'),
+//       eDelayTime = eDelayTime.split('s'),
+//       eDelayTimeNum = (Number(eDelayTime[0]) + 1) * 1000;
+//   setTimeout(() => {
+//       $('.sub_trans').addClass('on');
+//       $('.sec_2').addClass('on');
+//   }, eDelayTimeNum); 
+
+//   var $path = $("#grad_line").find(".circle_line");
+//   if (!$path.length) return;
+
+//   var el = $path.get(0);
+//   var totalLength = el.getTotalLength();
+
+//   $path.css({
+//       "stroke-dasharray": totalLength,
+//       "stroke-dashoffset": totalLength / 1.12
+//   });
+  
+//   var stepOffsets = [
+//     totalLength * 0.893,                // studio_n1 - 0%
+//     totalLength * 0.64,         // studio_n2 - 25%
+//     totalLength * 0.426,          // studio_n3 - 50%
+//     totalLength * 0.2,         // studio_n4 - 75%
+//     0                           // studio_n5 - 100%
+//   ];
+//   var studio = $('.studio');
+//   var bigLogo = $('.big_logo_wrap img');
+//   var subText = $('.sub_txt');
+//   $.each(studio, function(idx, item){
+//     $(this).click(function(){
+//       bigLogo.removeClass('on');
+//       bigLogo.eq(idx).addClass('on');
+  
+//       subText.removeClass('on');
+//       subText.eq(idx).addClass('on');
+  
+//       studio.removeClass('on');
+//       $(this).addClass('on');
+  
+//       // stroke 이동
+//       path.css({
+//         "stroke-dashoffset": stepOffsets[idx]
+//       });
+//     });
+//   });
+
+
+// });
